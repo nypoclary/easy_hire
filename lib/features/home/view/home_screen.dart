1e67cab25a29c25916d987c2df0a135677f53529
Claryngweyee <161452606+Claryquinnzle22@users.noreply.github.com>
2025-05-19 03:06:29 +0700

    job_search_screen & the rest home_screen

--- lib/features/home/view/home_screen.dart
+++ lib/features/home/view/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:easy_hire/features/home/widgets/job_filter_options.dart';
 import 'package:easy_hire/features/home/widgets/location_filter_chip.dart';
 import 'package:flutter/material.dart';
 import 'package:easy_hire/core/widgets/header.dart';
+import 'package:easy_hire/core/widgets/job_card.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -42,8 +43,7 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             SizedBox(height: 20),
             JobFilterOptions(),
-            const SizedBox(height: 24), // space below the cards
-
+            const SizedBox(height: 12),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20),
               child: Text(
@@ -53,6 +53,33 @@ class _HomeScreenState extends State<HomeScreen> {
                   fontWeight: FontWeight.bold,
                   color: Colors.black87,
                 ),
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                children: [
+                  JobCardWidget(
+                    role: 'Product Designer',
+                    company: 'Google inc  California, USA',
+                    salary: '\$15K',
+                    tags: ['Home Service', 'Remote'],
+                    imageAsset: 'assets/images/profile_pic.jpg',
+                    onTap: () {
+                      debugPrint('🟣 Card tapped!');
+                    },
+                  ),
+                  JobCardWidget(
+                    role: 'Mobile Dev',
+                    company: 'Google inc  California, USA',
+                    salary: '\$14K',
+                    tags: ['Flutter', 'Remote'],
+                    imageAsset: 'assets/images/profile_pic.jpg',
+                    onTap: () {
+                      debugPrint('🟣 Card tapped!');
+                    },
+                  ),
+                ],
               ),
             ),
           ],
--- lib/features/job_search/view/job_search_screen.dart
+++ lib/features/job_search/view/job_search_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:easy_hire/core/widgets/header.dart';
+import 'package:easy_hire/core/widgets/job_search_bar.dart';
+import 'package:easy_hire/features/home/widgets/location_filter_chip.dart';
+import 'package:easy_hire/core/widgets/job_card.dart';
 
 class JobSearchScreen extends StatefulWidget {
   const JobSearchScreen({super.key});
@@ -8,13 +12,94 @@ class JobSearchScreen extends StatefulWidget {
 }
 
 class _JobSearchScreenState extends State<JobSearchScreen> {
+  String selected = 'Yangon';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'This is search screen',
-          style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Header(title: 'All Jobs'),
+            SizedBox(height: 24),
+            JobSearchBar(
+              onChanged: (query) {
+                //  Add search/filter logic here
+                print("Search input: $query");
+              },
+            ),
+            SizedBox(height: 20),
+            LocationFilterChip(
+              selectedLocation: selected,
+              locationOptions: ['All', 'Yangon', 'Mandalay', 'Naypyitaw'],
+              onChanged: (value) {
+                setState(() {
+                  selected = value!;
+                  // Then filter your cards here
+                });
+              },
+            ),
+            SizedBox(height: 8),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Text(
+                'All Jobs Available',
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black87,
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                children: [
+                  JobCardWidget(
+                    role: 'Product Designer',
+                    company: 'Google inc  California, USA',
+                    salary: '\$15K',
+                    tags: ['Home Service', 'Remote'],
+                    imageAsset: 'assets/images/profile_pic.jpg',
+                    onTap: () {
+                      debugPrint('🟣 Card tapped!');
+                    },
+                  ),
+                  JobCardWidget(
+                    role: 'Mobile Dev',
+                    company: 'Google inc  California, USA',
+                    salary: '\$14K',
+                    tags: ['Flutter', 'Remote'],
+                    imageAsset: 'assets/images/profile_pic.jpg',
+                    onTap: () {
+                      debugPrint('🟣 Card tapped!');
+                    },
+                  ),
+                  JobCardWidget(
+                    role: 'Product Designer',
+                    company: 'Google inc  California, USA',
+                    salary: '\$15K',
+                    tags: ['Home Service', 'Remote'],
+                    imageAsset: 'assets/images/profile_pic.jpg',
+                    onTap: () {
+                      debugPrint('🟣 Card tapped!');
+                    },
+                  ),
+                  JobCardWidget(
+                    role: 'Mobile Dev',
+                    company: 'Google inc  California, USA',
+                    salary: '\$14K',
+                    tags: ['Flutter', 'Remote'],
+                    imageAsset: 'assets/images/profile_pic.jpg',
+                    onTap: () {
+                      debugPrint('🟣 Card tapped!');
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
